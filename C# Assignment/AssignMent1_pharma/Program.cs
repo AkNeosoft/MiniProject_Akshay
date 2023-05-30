64825bcae1f3b314cc3edf198ca228ea111655e8
AkNeosoft <132550257+AkNeosoft@users.noreply.github.com>
2023-05-30 13:56:03 +0530

    C#_Assignment

---
+++ C# Assignment/AssignMent1_pharma/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Globalization;
+using System.Xml.Linq;
+
+public class Boxing
+{
+    public static void Main(string[] args)
+    {
+        Medicine med = new Medicine("001","Dolo","Akshay","ASD","1000","22/05/2023","22/05/2024","1234");
+        med.display();
+
+        Sales sales = new Sales();
+    }
+
+    public class Medicine
+    {
+        public string MedicineCode { get; set; }
+        public string MedicineName { get; set; }
+
+        public string ManufacturarName { get; set; }
+
+        public string UnitPrice { get; set; }
+
+        public string QuantityOnhand { get; set; }
+
+        public string ManufactureDdate { get; set; }
+
+        public string ExpiryDate { get; set; }
+
+        public string BatchNumber { get; set; }
+
+
+        public Medicine(string medcode,string medname, string manufaturename, string unitprice,string quantity, string manufaturedate,string expiry, string batchnumber)
+        {
+
+            Accept(medcode,medname,manufaturename,unitprice,quantity, manufaturedate,expiry,batchnumber);
+            
+
+        }
+        public void Accept(string medcode, string medname, string manufaturename, string unitprice, string quantity, string manufaturedate, string expiry, string batchnumber)
+        {
+            MedicineCode = medcode;
+            MedicineName = medname;
+            ManufacturarName = manufaturename;
+            UnitPrice = unitprice;
+            QuantityOnhand = quantity;
+            ManufactureDdate = manufaturedate;
+            ExpiryDate = expiry;
+            BatchNumber = batchnumber;
+        }
+
+        public void display()
+        {
+            Console.WriteLine(MedicineCode + " " + MedicineName + " " + ManufacturarName + " " + UnitPrice + " " + QuantityOnhand + " " + ExpiryDate + " " + BatchNumber);
+        }
+
+    }
+
+    public class Sales
+    {
+
+    }
+}
---
+++ C# Assignment/Day3-C#/Program.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections;
+
+/// <summary>
+/// Assignment - Array List
+/// </summary>
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ArrayList patientNames = new ArrayList();
+
+        string name;
+        string option;
+
+        do
+        {
+            Console.WriteLine("Enter a patient name:");
+            name = Console.ReadLine();
+            patientNames.Add(name);
+
+            Console.WriteLine("Do you want to add more names? (y/n)");
+            option = Console.ReadLine();
+        }
+        while (option.ToLower() == "y");
+
+        Console.WriteLine("Patient names in the list:");
+        foreach (var patientName in patientNames)
+        {
+            Console.WriteLine(patientName);
+        }
+
+        Console.WriteLine("\nSelect one of the below options:");
+        Console.WriteLine("1. Sort the list");
+        Console.WriteLine("2. Reverse the list");
+        Console.WriteLine("3. Remove a name");
+        Console.WriteLine("4. Search for a name");
+        Console.WriteLine("5. Exit");
+
+        int Option;
+
+        while (true)
+        {
+            Console.WriteLine("\nEnter your option (1-5):");
+            if (int.TryParse(Console.ReadLine(), out Option))
+            {
+                if (Option == 1)
+                {
+                    patientNames.Sort();
+                    foreach (var patientName in patientNames)
+                    {
+                        Console.WriteLine(patientName);
+                    }
+
+                }
+                else if (Option == 2)
+                {
+                    patientNames.Reverse();
+                    foreach (var patientName in patientNames)
+                    {
+                        Console.WriteLine(patientName);
+                    }
+                }
+                else if (Option == 3)
+                {
+                    Console.WriteLine("Enter the name to remove:");
+                    string nameToRemove = Console.ReadLine();
+                    patientNames.Remove(nameToRemove);
+                    Console.WriteLine("Name removed.");
+                }
+                else if (Option == 4)
+                {
+                    Console.WriteLine("Enter the name to search:");
+                    string nameToSearch = Console.ReadLine();
+                    bool found = patientNames.Contains(nameToSearch);
+                    Console.WriteLine("Name found: " + found);
+                }
+                else if (Option == 5)
+                {
+                    Console.WriteLine("Exit");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid option");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+        }
+    }
+
+}
